1258b0f5d4a021383cfcfd4c3ef25f2ac02503c8
KamelJbo <kameljbo@gmail.com>
2014-09-06 23:19:58 -0400

    Agregué dos funciones
    Agregue la función de Sale y agregue la funcion de test

--- RisePay.java
+++ RisePay.java
@@ -1,5 +1,6 @@
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /*
@@ -29,6 +30,40 @@ public class RisePay {
     public void post(){
         
     }
-
+public void test(){
+    Map opt = new HashMap();
+    opt.put("NameOnCard","Kamel");
+    opt.put("CardNum", "5149612222222229");
+    opt.put("ExpDate", "1214");
+    opt.put("Amount", "10");
+    opt.put("CVNum", "734");
+    opt.put("InvNum", "ABD42");
+    opt.put("Zip", "36124");
+    opt.put("Street", "Gran Vio 25");
+    sale(opt);
+    }     
     
-}
+   public void sale(Map opt){ 
+        Map form = new HashMap();
+        form.put("UserName",username);
+        form.put("Password",password);
+        form.put("TransType","SALE");
+        form.put("NameOnCard", opt.get(0).toString());
+        form.put("CardNum", opt.get(1).toString());
+        form.put("ExpDate", opt.get(2).toString());
+        form.put("Amount", opt.get(3).toString());
+        form.put("MagData","");
+        form.put("PNRef","");
+        form.put("ExtData", "");     
+        form.put("CVNum",opt.get(4).toString());
+        form.put("InvNum",opt.get(5).toString());
+        form.put("Zip",opt.get(6).toString());
+        form.put("Street",opt.get(7).toString());
+        
+        for (Iterator it = form.keySet().iterator(); it.hasNext();) {
+            Object key = it.next();
+            System.out.println("key: " + key + "|  value: " + form.get(key));
+        }
+    
+        
+   }}
