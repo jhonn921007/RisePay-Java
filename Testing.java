2c6d70300d2ef9603759c7c5ffc2666aac7c3d1b
JhonnatanRodriguez <jhonn@outlook.com>
2014-09-07 01:48:36 -0400

    add Testing

---
+++ Testing.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javax.net.ssl.HttpsURLConnection;
+import org.javalite.http.Get;
+import org.javalite.http.Http;
+import org.javalite.http.Post;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jhonn
+ */
+public class Testing {
+    
+    public void test() throws IOException{
+        String content = "UserName=jhonndev&Password=U0H464z4";
+Post post = Http.post("https://gateway1.risepay.com/ws/transact.asmx/ProcessCreditCard?", content).header("Content-Type", "application/x-www-form-urlencoded");
+System.out.println(post.text());
+System.out.println(post.headers());
+System.out.println(post.responseCode());
+    }
+}
